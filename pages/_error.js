68cbbb46a92a19530f18508882208a452c8b9bfe
jungsikjeong <wndtlr1024@gmail.com>
2020-07-13 22:11:52 +0900

    getInitialProps로 동적데이터 활용하기

---
+++ pages/_error.js
@@ -0,0 +1,9 @@
+const { default: Layout } = require("../components/Layout");
+
+import Layout from "../components/Layout";
+
+export default () => (
+  <Layout title="Error!!!">
+    <p>해당 페이지를 가져올 수 없습니다. 죄송합니다.</p>
+  </Layout>
+);
--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { Component, useEffect, useState } from "react";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 import Layout from "../components/Layout";
 
-const about = () => {
+const About = (props) => {
+  About.getInitialProps = async () => {
+    const res = await fetch("https://api.github.com/users/jungsikjeong");
+    const data = await res.json();
+
+    return { user: data };
+  };
+
+  // useEffect(() => {
+  // fetch("https://api.github.com/users/reedbarger")
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     setUser({
+  //       user: data,
+  //     });
+  //   });
+  // }, []);
+
+  const { user } = props;
   return (
     <Layout title="About">
+      <p>{user.name}</p>
+
       <p>A javaScript programmer</p>
-      <img src="/static/javaScript-logo.png" alt="javascript" height="200px" />
+      <img src={user.avatar_url} alt="Reed" height="200px" />
     </Layout>
   );
 };
 
-export default about;
+export default About;
